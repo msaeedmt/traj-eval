dd84107a71b45c9fb049a3250ff7e4b59c826eb4
hanjiadong0 <hanjiadong0@gmail.com>
2026-07-14 20:09:23 +0200

    Archive Stargazer artifacts outside Lean branch

--- notebooks/qwen_saeed_stargazer_real1/support/stargazer/__init__.py
+++
@@ -1,5 +0,0 @@
-from .config import Task
-from .benchmark import evaluate_stargazer_benchmark
-from .evaluator import evaluate_submission
-
-__all__ = ["Task", "evaluate_stargazer_benchmark", "evaluate_submission"]
